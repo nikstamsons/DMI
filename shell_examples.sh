f1a95640e409725e4bef06ff5931d1fdafbe5e7f
v.smetskaja@gmail.com <nikstamsons@gmail.com>
2017-09-28 09:49:41 +0300

    20170921

---
+++ shell_examples.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#NAME="vards uzvards"
+#echo $NAME
+#NAME="vards uzvards"
+#echo $NAME
+#readonly NAME
+#NAME="uzvards vards"
+#echo $NAME
+#NAME="vards uzvards"
+#echo $NAME
+#NAME="vards uzvards"
+#echo $NAME
+#unset NAME
+#echo $NAME
+echo "atbilde uz jaut- kas tagad tiek pildits?" $0
+echo "skriptam nodoto argumentu skaits" $#

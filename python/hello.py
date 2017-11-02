cee735309c3d3b1187908ea41465a571ff8bef68
ansiiis <nikstamsons@gmail.com>
2017-11-02 11:34:19 +0200

    2017_01_11

---
+++ python/hello.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+print "Hello World!"
+print "Here \bthe \bspaces \bare \bbackspaced."
+print "Here \nthe \nspaces \nare \nnewlined."
+print "Here \tthe \tspaces \thave \thorizontal \ttab \tspaces."
+print "Here \vthe \vspaces \vhave \vvertical \vtab \vspaces."
+print "Here the \rcontent before this is removed."
+print "Sesi \vmazi \vbundzenieki \vjaj \vpa \vcelu \vbungodami"
+print "                                        Sesi"
+print "                                   mazi"   
+print "                       bundzenieki" 
+print "                   jaj"
+print "                pa"
+print "           celu"   
+print " bungodami"
+x = int(input("Input x: "))
+y = int(input("input y:"))
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)

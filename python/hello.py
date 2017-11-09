a0629df22b45f0127242f4d13a95539a36e16f4c
ansiiis <nikstamsons@gmail.com>
2017-11-09 09:54:21 +0200

    history2017_09_11.txt

--- python/hello.py
+++ python/hello.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
 print "Hello World!"
 print "Here \bthe \bspaces \bare \bbackspaced."
 print "Here \nthe \nspaces \nare \nnewlined."
@@ -13,11 +15,31 @@ print "                   jaj"
 print "                pa"
 print "           celu"   
 print " bungodami"
-x = int(input("Input x: "))
-y = int(input("input y:"))
+'''
+x = int(input("Ievadi x: "))
+y = int(input("Ievadi y:"))
 
 print(x + y)
 print(x - y)
 print(x * y)
 print(x / y)
 print(x % y)
+'''
+a = int(input("Ievadi x: "))
+print "tu ievadiji lielumu ar %s datu tipu"%(type(a))
+print a *a
+a=65
+print type(a)
+print "a kā dec: %d"%(a)
+print "a kā heks:%x"%(a)
+print "a kā oct: %o"%(a)
+print "a kā simbols: %c"%(a)
+
+a=8.5
+print type(a)
+print "a kā dec: %d"%(a)
+print "a kā heks:%x"%(a)
+print "a kā oct: %o"%(a)
+print "a kā reals skaitlis: %10.3f"%(a)
+'''
+
